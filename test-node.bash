a29b5efd068ff03f97b15b88b3c33dc58104b511
Ethen Pociask <ethenpociask@macbook-pro.mynetworksettings.com>
2025-06-25 19:34:07 -0400

    use nitro-node-dev

--- test-node.bash
+++ test-node.bash
@@ -2,7 +2,7 @@
 
 set -eu
 
-NITRO_NODE_VERSION=ghcr.io/layr-labs/nitro-eigenda:ethenotethan--update-eigenda-version
+NITRO_NODE_VERSION=ghcr.io/layr-labs/nitro/nitro-node-dev:ethenotethan--update-eigenda-version
 BLOCKSCOUT_VERSION=offchainlabs/blockscout:v1.1.0-0e716c8
 
 DEFAULT_NITRO_CONTRACTS_VERSION="eigenda-v3.1.0"
